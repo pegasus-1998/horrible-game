13f0fa6e9d55a81569697d371fc1d65697e0f38a
pgs <2226984343@qq.com>
2022-10-27 00:19:41 +0800

    事件封装

--- js/controlsMusic.js
+++ js/controlsMusic.js
@@ -1,16 +1,16 @@
-let playFlag = false  // true: 播放 false: 暂停
-const navMusic = document.querySelector('.nav-music')
-const audioEl = document.querySelector('.audio-el')
-const musicImg = document.querySelector('.music-img')
-navMusic.addEventListener('click', function() {
-    playFlag = !playFlag
-    if(playFlag) {
-        audioEl.play()
-        musicImg.src = './imgs/icon/music.png'
-    }else {
-        audioEl.pause()
-        musicImg.src = './imgs/icon/close-music.png'
-    }
-})
-
-const aaaa = 10
+(function () {
+    let playFlag = false  // true: 播放 false: 暂停
+    const navMusic = document.querySelector('.nav-music')
+    const audioEl = document.querySelector('.audio-el')
+    const musicImg = document.querySelector('.music-img')
+    navMusic.addEventListener('click', function () {
+        playFlag = !playFlag
+        if (playFlag) {
+            audioEl.play()
+            musicImg.src = './imgs/icon/music.png'
+        } else {
+            audioEl.pause()
+            musicImg.src = './imgs/icon/close-music.png'
+        }
+    })
+})()
--- js/index.js
+++ js/index.js
@@ -1,18 +1,89 @@
-let x = 0
-let y = 0
-let x2 = 0
-let y2 = 0
-let timer = null
-let playType = 1  // 1: 游戏未开始 2: 游戏进行中 3: 暂停游戏 
-const playGame = document.querySelector('.play-game')
-const playGameIcon = document.querySelector('.play-game-icon')
-const playGameText = document.querySelector('.play-game-text')
-const lovelyEl = document.querySelector('.lovely-gif')
-const monsterEl = document.querySelector('.monster-gif')
-initGameHandler()
-function initGameHandler() {
-    playGame.addEventListener('click', playGameHandler)
-    document.addEventListener('keydown', e => {
+(function () {
+    let x = 0
+    let y = 0
+    let x2 = 0
+    let y2 = 0
+    let timer = null
+    let playType = 1  // 1: 游戏未开始 2: 游戏进行中 3: 暂停游戏 
+    let currentLocation = 1
+    const monsterCurrentLocations = [25]
+    const grids = [
+        { x: 0, y: 0, curLocation: 1 },
+        { x: 120, y: 0, curLocation: 2 },
+        { x: 240, y: 0, curLocation: 3 },
+        { x: 360, y: 0, curLocation: 4 },
+        { x: 480, y: 0, curLocation: 5 },
+        { x: 0, y: 120, curLocation: 6 },
+        { x: 120, y: 120, curLocation: 7 },
+        { x: 240, Y: 120, curLocation: 8 },
+        { x: 360, y: 120, curLocation: 9 },
+        { x: 480, y: 120, curLocation: 10 },
+        { x: 0, y: 240, curLocation: 11 },
+        { x: 120, y: 240, curLocation: 12 },
+        { x: 240, y: 240, curLocation: 13 },
+        { x: 360, y: 240, curLocation: 14 },
+        { x: 480, y: 240, curLocation: 15 },
+        { x: 0, y: 360, curLocation: 16 },
+        { x: 120, y: 360, curLocation: 17 },
+        { x: 240, y: 360, curLocation: 18 },
+        { x: 360, y: 360, curLocation: 19 },
+        { x: 480, y: 360, curLocation: 20 },
+        { x: 0, y: 480, curLocation: 21 },
+        { x: 120, y: 480, curLocation: 22 },
+        { x: 240, y: 480, curLocation: 23 },
+        { x: 360, y: 480, curLocation: 24 },
+        { x: 480, y: 480, curLocation: 25 }
+    ]
+    const playGame = document.querySelector('.play-game')
+    const playGameIcon = document.querySelector('.play-game-icon')
+    const playGameText = document.querySelector('.play-game-text')
+    const lovelyEl = document.querySelector('.lovely-gif')
+    const monsterEl = document.querySelector('.monster-gif')
+
+    initGameHandler()
+
+
+    function initGameHandler() {
+        playGame.addEventListener('click', playGameHandler)
+    }
+
+    function playGameHandler() {
+        if (playType === 1) {
+            startGame()
+        } else if (playType === 2) {
+            pauseGame()
+        } else if (playType === 3) {
+            continueGame()
+        }
+    }
+
+    function startGame() {   // 开始游戏
+        playType = 2
+        playGameText.textContent = '暂停游戏'
+        playGameIcon.src = './imgs/icon/pause.png'
+        document.addEventListener('keydown', keyDownHandler)
+        timer = setInterval(() => monsterMoveOneSquare(), 500)
+    }
+
+    function pauseGame() {   // 暂停游戏
+        playType = 3
+        playGameText.textContent = '继续游戏'
+        playGameIcon.src = './imgs/icon/play.png'
+        document.removeEventListener('keydown', keyDownHandler)
+        clearInterval(timer)
+    }
+
+    function continueGame() {  // 继续游戏
+        playType = 2
+        playGameText.textContent = '暂停游戏'
+        playGameIcon.src = './imgs/icon/pause.png'
+        document.addEventListener('keydown', keyDownHandler)
+        timer = setInterval(() => {
+            monsterMoveOneSquare()
+        }, 500)
+    }
+
+    function keyDownHandler(e) {  // 键盘事件
         switch (e.key) {
             case 'ArrowUp':
                 lovelyMoveOneSquare(1)
@@ -27,61 +98,49 @@ function initGameHandler() {
                 lovelyMoveOneSquare(4)
                 break
         }
-    })
-}
-
-function playGameHandler() {
-    if (playType === 1) {
-        playType = 2
-        playGameText.textContent = '暂停游戏'
-        playGameIcon.src = './imgs/icon/pause.png'
-        timer = setInterval(() => {
-            monsterMoveOneSquare()
-        }, 1000)
-    }else if(playType === 2) {
-        playType = 3
-        playGameText.textContent = '继续游戏'
-        playGameIcon.src = './imgs/icon/play.png'
-        clearInterval(timer)
-    }else if(playType === 3) {
-        playType = 2
-        playGameText.textContent = '暂停游戏'
-        playGameIcon.src = './imgs/icon/pause.png'
-        timer = setInterval(() => {
-            monsterMoveOneSquare()
-        }, 1000)
     }
-}
 
-
-function lovelyMoveOneSquare(type) { // 1: top 2: right 3: down 4: left
-    if (type === 1 && y != 0) {
-        y -= 120
-    } else if (type === 2 && x != 480) {
-        x += 120
-    } else if (type === 3 && y != 480) {
-        y += 120
-    } else if (type === 4 && x != 0) {
-        x -= 120
+    function lovelyMoveOneSquare(type) { // 玩偶移动  1: top 2: right 3: down 4: left
+        if (type === 1 && y != 0) {
+            y -= 120
+        } else if (type === 2 && x != 480) {
+            x += 120
+        } else if (type === 3 && y != 480) {
+            y += 120
+        } else if (type === 4 && x != 0) {
+            x -= 120
+        }
+        for (const item of grids) {
+            if (x === item.x && y === item.y) {
+                currentLocation = item.curLocation
+                break
+            }
+        }
+        lovelyEl.style.transform = `translateX(${x}px) translateY(${y}px)`
     }
-    lovelyEl.style.transform = `translateX(${x}px) translateY(${y}px)`
-}
 
-function monsterMoveOneSquare() {
-    const type = randomNumDirection()
-    if (type == 1 && y2 != -480) {
-        y2 -= 120
-    } else if (type == 2 && x2 != 0) {
-        x2 += 120
-    } else if (type == 3 && y2 != 0) {
-        y2 += 120
-    } else if (type == 4 && x2 != -480) {
-        x2 -= 120
+    function monsterMoveOneSquare() {   // 怪物移动
+        const type = randomNumDirection()
+        if (type == 1 && y2 != -480) {
+            y2 -= 120
+        } else if (type == 2 && x2 != 0) {
+            x2 += 120
+        } else if (type == 3 && y2 != 0) {
+            y2 += 120
+        } else if (type == 4 && x2 != -480) {
+            x2 -= 120
+        }
+        for (const item of grids) {
+            if (x2 === item.x && y2 === item.y) {
+                monsterCurrentLocations[0] = item.curLocation
+                break
+            }
+        }
+        monsterEl.style.transform = `translateX(${x2}px) translateY(${y2}px)`
     }
-    monsterEl.style.transform = `translateX(${x2}px) translateY(${y2}px)`
-}
 
-function randomNumDirection() {  // 1: top 2: right 3: down 4: left
-    return Math.floor(Math.random() * 4 + 1)
-}
+    function randomNumDirection() {  // 怪物随机移动参数 1: top 2: right 3: down 4: left
+        return Math.floor(Math.random() * 4 + 1)
+    }
+})()
 
