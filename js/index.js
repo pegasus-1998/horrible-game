5ed31301c85a5eb6f88baaa9f3c85a6e19801334
pgs <2226984343@qq.com>
2022-11-02 00:47:01 +0800

    解决怪物位置重叠

--- js/index.js
+++ js/index.js
@@ -30,10 +30,6 @@
     ]
     const initMonsterXy = deepClone(monstersXY) 
 
-    let timer = null
-    let dountDown = 1000 // 怪物速度
-    let playType = 1  // 1: 游戏未开始 2: 游戏进行中 3: 暂停游戏 4: 游戏结束
-
     const grids = [     // 存储每个格子的位置
         { x: 0, y: 0, curLocation: 1 },
         { x: 120, y: 0, curLocation: 2 },
@@ -62,17 +58,22 @@
         { x: 480, y: 480, curLocation: 25 }
     ]
 
+    let timer = null
+    let passLevelTimer = null 
+    let dountDown = 1000 // 怪物速度
+    let playType = 1  // 1: 游戏未开始 2: 游戏进行中 3: 暂停游戏 4: 游戏结束
+
     const playGame = document.querySelector('.play-game')
     const playGameIcon = document.querySelector('.play-game-icon')
     const playGameText = document.querySelector('.play-game-text')
     const lovelyEl = document.querySelector('.lovely-gif')
     const monstersEl = document.querySelectorAll('.monster-gif')
     const levelItems = document.querySelectorAll('.level-item')
-    const hoverContainer = document.querySelector('.hover-show-container')
-    const navSelect = document.querySelector('.nav-select')
+    const countTimeEl = document.querySelector('.count-time')
 
     playGame.addEventListener('click', playGameHandler)
     seletLevel()
+    countDownPassLevel()
 
     function playGameHandler() {
         if (playType === 1 || playType === 4) {
@@ -177,18 +178,44 @@
         monstersXY.forEach((item, index) => {
             const type = randomNumDirection()
             if (type === 1 && item.y != 0) {
+                const flag = stopMonsterSameLovation(item.x, item.y - 120)
+                if(flag) {
+                    return 
+                }
                 item.y -= 120
             } else if (type === 2 && item.x != 480) {
+                const flag = stopMonsterSameLovation(item.x + 120, item.y)
+                if(flag) {
+                    return
+                }
                 item.x += 120
             } else if (type === 3 && item.y != 480) {
+                const flag = stopMonsterSameLovation(item.x, item.y + 120)
+                if(flag) {
+                    return
+                }
                 item.y += 120
             } else if (type === 4 && item.x != 0) {
+                const flag = stopMonsterSameLovation(item.x - 120, item.y)
+                if(flag) {
+                    return
+                }
                 item.x -= 120
             }
             item.currentLocation = queryGridLoca(item.x, item.y)
             monstersEl[index].style.transform = `translateX(${item.x}px) translateY(${item.y}px)`
         })
         sameLocation()
+    }
+
+    function stopMonsterSameLovation(mX, mY) {  // 禁止四个怪物位置重叠
+        const tempLoca = queryGridLoca(mX, mY) 
+        for(let i = 0; i < monstersXY.length; i++) {
+            const { currentLocation } = monstersXY[i]
+            if(tempLoca === currentLocation) {
+               return true
+            }
+        }
     }
 
     function queryGridLoca(x, y) {  // 根据xy轴查询格子
@@ -209,11 +236,22 @@
                 dountDown = 1000 - idx * 70
                 levelItems.forEach(sItem => sItem.className = 'level-item')
                 this.className = 'level-item level-active'
-                hoverContainer.style.display = 'none'
                 initGame(true)
             })
         })
-        navSelect.addEventListener('click', () => hoverContainer.style.display = 'block')
+    }
+
+    function countDownPassLevel() {  // 倒计时通关
+        let count = 45
+        passLevelTimer = setInterval(() => {
+            if(count === 0) {
+                clearInterval(passLevelTimer)
+            }else {
+                count--
+                const temp = count < 10 ? '0' + count : count
+                countTimeEl.textContent = temp
+            }
+        }, 100)
     }
 
     function deepClone(obj) {   // 深拷贝
